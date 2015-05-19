299315002b6f64abf32101dac09e402d77d516de
Frank B <fb.fbook@gmail.com>
2015-05-19 13:32:43 -0400

    a folder for practice challenges

---
+++ unique_character_string/code.rb
@@ -0,0 +1,19 @@
+def unique_characters?(string_arg)
+	white_space_removed = string_arg.gsub(/\s/, '')
+	string_array = white_space_removed.downcase.split('')
+	counter = 0
+	uniqueness_test = []
+	string_array.each do |x|
+		string_array.each do |y|
+			if x == y
+				counter += 1
+			end
+		end
+	end
+	puts counter
+	if counter == string_array.length
+		true
+	else
+		false
+	end
+end
---
+++ unique_character_string/readme.md
@@ -0,0 +1,9 @@
+This is practice and fun.
+
+Write an method to check a string for uniqueness of its individual characters.
+
+questions/assumptions:
+-Does white space count? -no
+-Does punctuation count? -yes
+-Does case matter (upper or lower) -no
+-Do not use the 'Set' data structure
